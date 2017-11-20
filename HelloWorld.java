b672dd2949405de471317285447be8643eaa3c38
GIT SHEU <wrsheu@gmail.com>
2017-11-19 18:14:42 -0800

    modify HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		for (int i=1; i<=10; i++) {
+		for (int i=1; i<=5; i++) {
 			System.out.println("Hello World " + i + " times");
 		}
 	}
